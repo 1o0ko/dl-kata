fa818909a23fd69ab77be725c6966f07d60de787
1o0ko <wojciech.stokowiec@gmail.com>
2018-01-14 19:42:33 -0500

    added one-hot-encoding

--- tiny-flow/example_mnist.py
+++ tiny-flow/example_mnist.py
@@ -23,20 +23,29 @@ def flatten(X):
     return X.reshape(n, h * w)
 
 
+def one_hot(X, n=None):
+    n = n if n else np.max(X) + 1
+    one_hot_encoding = np.zeros([X.shape[-1], n])
+    one_hot_encoding[np.arange(X.shape[-1]), X] = 1
+
+    return one_hot_encoding
+
+
 def main():
     # create pipeline
-    pipeline = normalize >> flatten
+    image_pipeline = normalize >> flatten
+    label_pipeline = one_hot
 
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
-    logger.info("Loading mnist")
-    train_images = pipeline(mnist.train_images())
-    train_labels = mnist.train_labels()
-    logger.info("Train: %s, %s", train_images.shape, train_labels.shape)
-    logger.info(train_images[0], train_labels[0])
 
-    test_images = pipeline(mnist.test_images())
-    test_labels = mnist.test_labels()
+    logger.info("Loading mnist")
+    train_images = image_pipeline(mnist.train_images())
+    train_labels = label_pipeline(mnist.train_labels())
+    logger.info("Train: %s, %s", train_images.shape, train_labels.shape)
+
+    test_images = image_pipeline(mnist.test_images())
+    test_labels = label_pipeline(mnist.test_labels())
     logger.info("Test: %s, %s", test_images.shape, test_labels.shape)
     logger.info("Done!")
 
